23a5d7848d5c9590e484c9359ab2d944a5cdd13e
miraphira <fadhilahintan01@gmail.com>
2026-07-03 08:34:15 +0700

    Improved Project Detail

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Portfolio online!")
+const buttons = document.querySelectorAll(".detail-btn");
+buttons.forEach(button => {
+    button.onclick = function(event){
+        event.preventDefault();
+        const modalId = button.dataset.modal;
+        const modal = document.getElementById(modalId);
+        modal.style.display = "block";
+    }
+});
+
+const closeButtons = document.querySelectorAll(".close-btn");
+closeButtons.forEach(button => {
+    button.onclick = function(){
+        button.closest(".modal").style.display = "none";
+    }
+});
